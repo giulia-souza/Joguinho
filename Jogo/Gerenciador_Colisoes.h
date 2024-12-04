32a0c8cbc96052ed75d85895eb76787b806390db
Giulia <giulia.souza.leite.04@gmail.com>
2024-12-04 16:26:12 -0300

    lista.h feita com implementações e tbm listaEntidades.h feita
    prox passo, fazer o entidade, jogador, inimigo, obst

---
+++ Jogo/Gerenciador_Colisoes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Entidades/Jogador.h"
+#include "Entidades/Inimigo.h"
+#include "Entidades/Fogo_Magico.h"//projetil
+#include "Entidades/Obstaculo.h"
+
+#define CR 0.30 //coef de restituicao
+
+namespace Gerenciadores {
+	class Gerenciador_Colisoes {
+	private:
+		//listas...
+	public:
+		//....
+	};
+}
